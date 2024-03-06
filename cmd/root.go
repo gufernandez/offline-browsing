63cb4ae64c9d14192907118a39c32d53e7d60226
Gustavo Fernandez <gustavo.fernandez.costa@gmail.com>
2024-03-06 19:42:10 -0300

    feature: html download

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,30 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "offline-browsing",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "fetch",
+	Short: "Download your webpage for offline use.",
+	Long: `Use this application to download all webpages that you like. 
+	
+	The informed URLs will be downloaded to the current folder as html files.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		root(args)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -41,11 +41,38 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.offline-browsing.yaml)")
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gotest.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
+func root(args []string) {
+	for _, value := range args {
+		fmt.Println(value)
+		fetch(value)
+	}
+}
 
+func fetch(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	var splitted = strings.Split(url, "//")
+	var filename = splitted[1] + ".html"
+
+	f, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	_, err = io.Copy(f, resp.Body)
+	if err != nil {
+		panic(err)
+	}
+}
