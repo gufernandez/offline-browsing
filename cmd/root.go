a8165bd3316f7dae20b2ae318f5c202033ffd882
Gustavo Fernandez <gustavo.fernandez.costa@gmail.com>
2024-03-06 20:50:16 -0300

    feature: setting metadata in html

--- cmd/root.go
+++ cmd/root.go
@@ -1,34 +1,31 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "fetch",
 	Short: "Download your webpage for offline use.",
 	Long: `Use this application to download all webpages that you like. 
 	
 	The informed URLs will be downloaded to the current folder as html files.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
-		root(args)
+		showMetadata, _ := cmd.Flags().GetBool("metadata")
+		root(args, showMetadata)
 	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -36,24 +33,21 @@ func Execute() {
 	}
 }
 
+// Flags definition
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gotest.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolP("metadata", "m", false, "Show metadata of existing file. If non existent, will be downloaded.")
 }
 
-func root(args []string) {
-	for _, value := range args {
-		fmt.Println(value)
-		fetch(value)
+// Root function to handle args
+func root(urls []string, showMetadata bool) {
+	for _, url := range urls {
+		fetch(url)
 	}
 }
+
+// func getMetadata(filename string) {
+
+// }
 
 func fetch(url string) {
 	resp, err := http.Get(url)
@@ -62,9 +56,7 @@ func fetch(url string) {
 	}
 	defer resp.Body.Close()
 
-	var splitted = strings.Split(url, "//")
-	var filename = splitted[1] + ".html"
-
+	filename := urlFilename(url)
 	f, err := os.Create(filename)
 	if err != nil {
 		panic(err)
@@ -75,4 +67,48 @@ func fetch(url string) {
 	if err != nil {
 		panic(err)
 	}
+
+	setMetadata(url)
+}
+
+func setMetadata(url string) {
+	buf := bytes.NewBuffer(nil)
+	f, err := os.OpenFile(urlFilename(url), os.O_APPEND|os.O_RDWR, 0644)
+	io.Copy(buf, f)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	s := buf.String()
+	dt := time.Now()
+
+	siteMeta := metadataFormat("site", url)
+	linksMeta := metadataFormat("num_links", getLinksQuantity(s))
+	imagesMeta := metadataFormat("images", getImagesQuantity(s))
+	dateMeta := metadataFormat("last_fetch", dt.Format(time.RFC1123))
+	toAppend := siteMeta + linksMeta + imagesMeta + dateMeta
+	if _, err := f.WriteString(toAppend); err != nil {
+		panic(err)
+	}
+}
+
+func getLinksQuantity(fileContent string) string {
+	r, _ := regexp.Compile("(href=\"https)")
+	match := r.FindAllString(fileContent, -1)
+	return fmt.Sprintf("%d", len(match))
+}
+
+func getImagesQuantity(fileContent string) string {
+	r, _ := regexp.Compile("(<img)")
+	match := r.FindAllString(fileContent, -1)
+	return fmt.Sprintf("%d", len(match))
+}
+
+func metadataFormat(name string, content string) string {
+	return "\n<meta name=\"" + name + "\" content=\"" + content + "\">"
+}
+
+func urlFilename(url string) string {
+	return strings.Split(url, "//")[1] + ".html"
 }
