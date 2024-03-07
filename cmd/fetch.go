30d85b9df830b81581f359e0e1e1f72a843b058f
Gustavo Fernandez <gustavo.fernandez.costa@gmail.com>
2024-03-06 22:47:16 -0300

    feature: download images

--- cmd/fetch.go
+++ cmd/fetch.go
@@ -32,7 +32,8 @@ var fetchCmd = &cobra.Command{
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		showMetadata, _ := cmd.Flags().GetBool("metadata")
-		root(args, showMetadata)
+		fullDownload, _ := cmd.Flags().GetBool("full-download")
+		root(args, showMetadata, fullDownload)
 	},
 }
 
@@ -47,36 +48,42 @@ func Execute() {
 // Flags definition
 func init() {
 	fetchCmd.Flags().BoolP("metadata", "m", false, "Show metadata of existing file. If non existent, will be downloaded.")
+	fetchCmd.Flags().BoolP("full-download", "f", false, "Download all images from the HTML for total offline use")
 }
 
 // Root function to handle args
-func root(urls []string, showMetadata bool) {
+func root(urls []string, showMetadata bool, fullDownload bool) {
 	for _, url := range urls {
 		fetchLink := formatLink(url)
 		if showMetadata {
 			if _, err := os.Stat(fetchLink.filename); os.IsNotExist(err) {
 				fmt.Println("-- Downloading Now")
-				fetch(fetchLink)
-				printMetadata(fetchLink.filename)
+				fetch(fetchLink, fullDownload)
+				printFileMetadata(fetchLink.filename)
 			} else {
 				fmt.Println("-- Already Downloaded")
-				printMetadata(fetchLink.filename)
+				printFileMetadata(fetchLink.filename)
 			}
 		} else {
-			fetch(fetchLink)
+			fetch(fetchLink, fullDownload)
 		}
 		fmt.Println()
 	}
 }
 
-func fetch(fetchLink weblink) {
-	resp, err := http.Get(fetchLink.url)
+func fetch(fetchLink weblink, downloadAll bool) {
+	saveLinkContentToFile(fetchLink.url, fetchLink.filename)
+	setFileMetadataAndImages(fetchLink, downloadAll)
+}
+
+func saveLinkContentToFile(url string, filepath string) {
+	resp, err := http.Get(url)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 
-	f, err := os.Create(fetchLink.filename)
+	f, err := os.Create(filepath)
 	if err != nil {
 		panic(err)
 	}
@@ -86,11 +93,9 @@ func fetch(fetchLink weblink) {
 	if err != nil {
 		panic(err)
 	}
-
-	setMetadata(fetchLink)
 }
 
-func printMetadata(filename string) {
+func printFileMetadata(filename string) {
 	buf := bytes.NewBuffer(nil)
 	f, err := os.OpenFile(filename, os.O_RDWR, 0644)
 	io.Copy(buf, f)
@@ -108,38 +113,73 @@ func printMetadata(filename string) {
 	}
 }
 
-func setMetadata(fetchLink weblink) {
+func setFileMetadataAndImages(fetchLink weblink, fetchImages bool) {
 	buf := bytes.NewBuffer(nil)
-	f, err := os.OpenFile(fetchLink.filename, os.O_APPEND|os.O_RDWR, 0644)
+	f, err := os.OpenFile(fetchLink.filename, os.O_RDWR|os.O_APPEND, 0644)
 	io.Copy(buf, f)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
+	fileContent := buf.String()
 
-	s := buf.String()
-	dt := time.Now()
+	imagesCount, imagesLinks := getImagesLinks(fileContent)
 
-	siteMeta := metadataFormat("site", fetchLink.baseUrl)
-	linksMeta := metadataFormat("num_links", getLinksQuantity(s))
-	imagesMeta := metadataFormat("images", getImagesQuantity(s))
-	dateMeta := metadataFormat("last_fetch", dt.Format(time.RFC1123))
-	toAppend := siteMeta + linksMeta + imagesMeta + dateMeta
+	toAppend := metadataFormat("site", fetchLink.baseUrl) + metadataFormat("num_links", getLinksQuantity(fileContent)) + metadataFormat("images", imagesCount) + metadataFormat("last_fetch", time.Now().Format(time.RFC1123))
 	if _, err := f.WriteString(toAppend); err != nil {
+		panic(err)
+	}
+
+	if fetchImages {
+		updatedContent := downloadImages(imagesLinks, fetchLink, fileContent+toAppend)
+		updateFileContent(fetchLink.filename, updatedContent)
+	}
+
+}
+
+func updateFileContent(filepath string, content string) {
+	f, err := os.Create(filepath)
+	if err != nil {
+		panic(err)
+	}
+	_, err = fmt.Fprint(f, content)
+	if err != nil {
 		panic(err)
 	}
 }
 
+func downloadImages(linkList []string, pageLink weblink, fileContent string) string {
+	srcR, _ := regexp.Compile(`[^\/]\/([^\/]+\.[a-z]+)`)
+	os.Mkdir(pageLink.baseUrl, os.ModePerm)
+	for _, img := range linkList {
+		name := srcR.FindAllStringSubmatch(img, -1)
+		newSrc := pageLink.baseUrl + "/" + name[0][1]
+
+		imgUrl := img
+		if strings.HasPrefix(img, "/") {
+			imgUrl = pageLink.url + img
+		}
+
+		saveLinkContentToFile(imgUrl, newSrc)
+		fileContent = strings.ReplaceAll(fileContent, img, newSrc)
+	}
+	return fileContent
+}
+
 func getLinksQuantity(fileContent string) string {
-	r, _ := regexp.Compile("(href=\"https)")
+	r, _ := regexp.Compile("(href=\"http)")
 	match := r.FindAllString(fileContent, -1)
 	return fmt.Sprintf("%d", len(match))
 }
 
-func getImagesQuantity(fileContent string) string {
-	r, _ := regexp.Compile("(<img)")
-	match := r.FindAllString(fileContent, -1)
-	return fmt.Sprintf("%d", len(match))
+func getImagesLinks(fileContent string) (string, []string) {
+	r, _ := regexp.Compile(`<img[^>]*src="([^"]*)"`)
+	match := r.FindAllStringSubmatch(fileContent, -1)
+	linkList := make([]string, len(match))
+	for i := range match {
+		linkList[i] = match[i][1]
+	}
+	return fmt.Sprintf("%d", len(linkList)), linkList
 }
 
 func metadataFormat(name string, content string) string {
